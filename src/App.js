1e1747b158b025be0e89037edf3e9b845d5a428a
Aditya Sharat <adityasharat@gmail.com>
2019-01-29 11:59:25 +0000

    update the app with demo tree

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Tree from 'react-d3-tree';
 import './App.css';
+
+const data = [
+  {
+    name: 'Playground',
+    attributes: {
+      key: 'A',
+    },
+    children: [
+      {
+        name: 'Counter',
+        attributes: {
+          key: 'B',
+          state: JSON.stringify({ count: 0 })
+        },
+        children: [
+          {
+            name: 'Text',
+            attributes: {
+              key: 'C',
+              props: JSON.stringify({ text: 'Increment' })
+            },
+          },
+          {
+            name: 'Text',
+            attributes: {
+              key: 'C',
+              props: JSON.stringify({ text: '0' })
+            },
+          }    
+        ]
+      },
+      {
+        name: 'Text',
+        attributes: {
+          key: 'C',
+          props: JSON.stringify({ text: 'World' })
+        },
+      },
+    ],
+  },
+];
 
 class App extends Component {
   render() {
+    const scaleExtent = {min: 1, max: 3};
+    const separation = {siblings: 2, nonSiblings: 4};
+    const translate = {x: 800, y: 50};
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <div className="app">
+        <header className="app-container">
+          <div id="treeWrapper" style={{width: '1920px', height: '1440px'}}>
+            <Tree data={data} 
+                  orientation="vertical"
+                  zoomable="true"
+                  zoom="2"
+                  scaleExtent={scaleExtent}
+                  separation={separation}
+                  translate={translate}
+            />
+          </div>
         </header>
       </div>
     );
