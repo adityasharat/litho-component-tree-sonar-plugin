f7b71c7a8574d6aca787b2e5e48e728b644d3905
Aditya Sharat <adityasharat@gmail.com>
2019-01-31 16:08:10 +0000

    add some css

--- src/components/NodeLabel.js
+++ src/components/NodeLabel.js
@@ -1,11 +1,25 @@
 import React from 'react';
 
+function NodeAttribute(props) {
+  return (
+    <li className="node-attribute-row">
+      <p className="node-attribute-key">{props.name}: </p>
+      <p className="node-attribute-value">{props.value}</p>
+    </li>
+  );
+}
+
 function NodeLabel(props) {
   const node = props.nodeData;
   return (
-    <div className="">
+    <div className="node-container">
       <h4 className="node-name" title={node.name}>{node.name}</h4>
-      
+      <ul className="node-attributes">
+        {
+          Object.keys(node.attributes)
+            .map(key => <NodeAttribute name={key} value={node.attributes[key]}/>)
+        }
+      </ul>
     </div>
   );
 }
