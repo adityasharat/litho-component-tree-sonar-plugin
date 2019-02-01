ce98834bb4e95856f14496b820626be4b6472def
Aditya Sharat <adityasharat@gmail.com>
2019-02-01 11:42:11 +0000

    wire up event viewer

--- src/components/EventViewer.js
+++ src/components/EventViewer.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
 
+const STUB_DATA = [{"name":1},{"name":2},{"name":3},{"name":4},{"name":5},{"name":6},{"name":7},{"name":8},{"name":9},{"name":10}];
+
 class EventViewer extends Component {
   render() {
     return (
-      <div>
-        <h2>Events</h2>
+      <div className="eventviewer-container">
+        <h2 className="title">Events</h2>
+        <div className="events-container">
+          <ol className="events-list">
+            {
+              this.props.events.map(event => {
+                return (
+                  <li className="event-item" key={event.name}>{event.name}</li>
+                )
+              })
+            }
+          </ol>
+        </div>
       </div>
     );
   }
 }
+
+EventViewer.defaultProps = {
+  events: STUB_DATA
+};
 
 export default EventViewer;
