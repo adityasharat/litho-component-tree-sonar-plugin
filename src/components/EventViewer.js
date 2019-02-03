cc4c70a1a1454f3a665a6c8e8c4f68e070242450
Aditya Sharat <adityasharat@gmail.com>
2019-02-03 10:03:30 +0000

    bug fix

--- src/components/EventViewer.js
+++ src/components/EventViewer.js
@@ -6,9 +6,6 @@ const STUB_EVENTS = [{
 }, {
   "timestamp":1549130568544,
   "data":{"name":"PlaygroundComponent","attributes":{"key":"9","id":838,"global-key":"9","props":"{}","state":"{}"},"children":[{"name":"Counter","attributes":{"key":"11","id":813,"global-key":"9,10,11","props":"{}","state":"{}"},"children":[{"name":"Text","attributes":{"key":"8","id":838,"global-key":"9,10,11,10,8","props":"{}","state":"{}"},"children":[],"diff":{"state":0}},{"name":"Text","attributes":{"key":"8","id":839,"global-key":"9,10,11,10,8!1","props":"{}","state":"{}"},"children":[],"diff":{"state":0}}],"diff":{"state":-1}},{"name":"Text","attributes":{"key":"8","id":814,"global-key":"9,10,8","props":"{}","state":"{}"},"children":[],"diff":{"state":1}},{"name":"Counter","attributes":{"key":"11","id":815,"global-key":"9,10,11!1","props":"{}","state":"{}"},"children":[{"name":"Text","attributes":{"key":"8","id":822,"global-key":"9,10,11!1,10,8","props":"{}","state":"{}"},"children":[],"diff":{"state":1}},{"name":"Text","attributes":{"key":"8","id":823,"global-key":"9,10,11!1,10,8!1","props":"{}","state":"{}"},"children":[],"diff":{"state":1}}],"diff":{"state":1}}],"diff":{"state":1}}
-},{
-  "timestamp":1549131568544,
-  "data":{"name":"PlaygroundComponent","attributes":{"key":"9","id":854,"global-key":"9","props":"{}","state":"{}"},"children":[{"name":"Counter","attributes":{"key":"11","id":813,"global-key":"9,10,11","props":"{}","state":"{}"},"children":[{"name":"Text","attributes":{"key":"8","id":838,"global-key":"9,10,11,10,8","props":"{}","state":"{}"},"children":[],"diff":{"state":1}},{"name":"Text","attributes":{"key":"8","id":839,"global-key":"9,10,11,10,8!1","props":"{}","state":"{}"},"children":[],"diff":{"state":1}}],"diff":{"state":1}},{"name":"Text","attributes":{"key":"8","id":814,"global-key":"9,10,8","props":"{}","state":"{}"},"children":[],"diff":{"state":1}},{"name":"Counter","attributes":{"key":"11","id":815,"global-key":"9,10,11!1","props":"{}","state":"{}"},"children":[{"name":"Text","attributes":{"key":"8","id":854,"global-key":"9,10,11!1,10,8","props":"{}","state":"{}"},"children":[],"diff":{"state":0}},{"name":"Text","attributes":{"key":"8","id":855,"global-key":"9,10,11!1,10,8!1","props":"{}","state":"{}"},"children":[],"diff":{"state":0}}],"diff":{"state":-1}}],"diff":{"state":1}}
 }];
 
 const STUB_DATA = {"name":"PlaygroundComponent","attributes":{"key":"9","id":854,"global-key":"9","props":"{}","state":"{}"},"children":[{"name":"Counter","attributes":{"key":"11","id":813,"global-key":"9,10,11","props":"{}","state":"{}"},"children":[{"name":"Text","attributes":{"key":"8","id":838,"global-key":"9,10,11,10,8","props":"{}","state":"{}"},"children":[],"diff":{"state":1}},{"name":"Text","attributes":{"key":"8","id":839,"global-key":"9,10,11,10,8!1","props":"{}","state":"{}"},"children":[],"diff":{"state":1}}],"diff":{"state":1}},{"name":"Text","attributes":{"key":"8","id":814,"global-key":"9,10,8","props":"{}","state":"{}"},"children":[],"diff":{"state":1}},{"name":"Counter","attributes":{"key":"11","id":815,"global-key":"9,10,11!1","props":"{}","state":"{}"},"children":[{"name":"Text","attributes":{"key":"8","id":854,"global-key":"9,10,11!1,10,8","props":"{}","state":"{}"},"children":[],"diff":{"state":0}},{"name":"Text","attributes":{"key":"8","id":855,"global-key":"9,10,11!1,10,8!1","props":"{}","state":"{}"},"children":[],"diff":{"state":0}}],"diff":{"state":-1}}],"diff":{"state":1}}
@@ -36,13 +33,14 @@ class EventViewer extends Component {
     try {
       const value = this.content.current.value;
       const data = JSON.parse(value);
-      this.props.events.push({
+      this.state.events.push({
         timestamp: Number(new Date()),
         data
       });
       this.setState({
         events: this.state.events
       })
+      this.content.current.value = '';
     } catch (e) {
       console.error(e);
       alert(`not a valid json: ${e}`)
